c181431b3ca344679fa685ac0ae333d04066470d
Guilherme Bordignon <gzbordignon@gmail.com>
2020-09-03 16:28:38 -0300

    uibuild

---
+++ build/precache-manifest.91c1f88ef75cb2261a9f70d2748383b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd6d1c6a7a369ccb3aef61303d60ced6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aef518103d415929b302",
+    "url": "/static/js/2.c2ec8293.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2ec8293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "980411392abf3d03568c",
+    "url": "/static/js/main.682f7085.chunk.js"
+  },
+  {
+    "revision": "24dc2dfa4d58f97935cc",
+    "url": "/static/js/runtime-main.dae54192.js"
+  }
+]);
